79238156aad6969cf0047dead2c9f367d4e0d9b9
Gianluca Costa <gianluca@gianlucacosta.info>
2016-04-04 03:09:49 +0200

    Add GetCommandOutputString()

--- ipc.go
+++ ipc.go
@@ -5,12 +5,21 @@ import (
 )
 
 /*
-GetProcessOutputString runs the given app with the given arguments,
-returning its output as a string or an error on failure.
+GetProcessOutputString runs the given program with the given arguments,
+returning its output as a string - or an error on failure.
 */
 func GetProcessOutputString(name string, arg ...string) (outputString string, err error) {
-	bytes, err := exec.Command(name, arg...).Output()
+	command := exec.Command(name, arg...)
 
+	return GetCommandOutputString(command)
+}
+
+/*
+GetCommandOutputString runs the given command, returning its output
+as a string - or an error on failure.
+*/
+func GetCommandOutputString(command *exec.Cmd) (outputString string, err error) {
+	bytes, err := command.Output()
 	if err != nil {
 		return "", err
 	}
